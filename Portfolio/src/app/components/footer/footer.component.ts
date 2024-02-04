fae88aa732ed0f3dab5909a9cfdc53d5e340452a
Guadalupe <guadalupenicol102@gmail.com>
2024-02-03 22:58:34 -0300

    imagen start

--- Portfolio/src/app/components/footer/footer.component.ts
+++ Portfolio/src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  constructor(private router: Router) {}
+
+  redirect(page: string):void {
+    if (page === 'github') {
+      window.location.href = 'https://github.com/Ashinoa'; // O utiliza this.router.navigate si estás manejando rutas internas de Angular
+    }else if(page === 'linkedin'){
+      window.location.href = 'https://www.linkedin.com';
+    }else{
+      window.location.href = 'https://mail.google.com/mail/?view=cm&fs=1&to=guadalupenicolearroyo@gmail.com';
+    }
+  }
 
 }
